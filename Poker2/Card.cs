bad9bff2c4725dd26e33b2bd23d005913f935cf2
bjiake <suyd@list.ru>
2022-04-11 23:14:39 +0300

    Добавьте файлы проекта.

---
+++ Poker2/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker2
+{
+    class Card
+    {
+        public enum SUIT
+        { 
+            Hearts,
+            Spades,
+            Diamonds,
+            Clubs
+        }
+        public enum VALUE
+        {
+            Two = 2, Three, Four, Ffive, Six, Seven, Eight, Nine,
+            Ttn, Jack, Queen, King, Ace
+        }
+        public SUIT MySuit { get; set; }
+        public VALUE MyValue { get; set; }
+    }
+}
